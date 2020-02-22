71f7beb3e1c45ec90f5ff0a8b8a36587acc835a8
eotkd4791 <eotkd4791@gmail.com>
2020-02-23 01:58:52 +0900

    Refactoring action in Vuex with async/await

--- src/api/index.js
+++ src/api/index.js
@@ -7,14 +7,23 @@ const config = {
 function fetchNewsList() {
   return axios.get(`${config.baseUrl}news/1.json`);
 }
-function fetchJobsList() {
-  return axios.get(`${config.baseUrl}jobs/1.json`);
+async function fetchJobsList() {
+  try {
+    const response = await axios.get(`${config.baseUrl}jobs/1.json`);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 }
 function fetchAskList() {
   return axios.get(`${config.baseUrl}ask/1.json`);
 }
-function fetchList(pageName) {
-  return axios.get(`${config.baseUrl}${pageName}/1.json`)
+async function fetchList(pageName) {
+  try {
+    return await axios.get(`${config.baseUrl}${pageName}/1.json`);
+  } catch (error) {
+    console.log(error);
+  }
 }
 function fetchUserInfo(username) {
   return axios.get(`${config.baseUrl}user/${username}.json`);
--- src/store/actions.js
+++ src/store/actions.js
@@ -8,63 +8,38 @@ import {
 } from '../api/index.js';
 
 export default {
-  FETCH_NEWS(context) {
-    return fetchNewsList()
-      .then(response => {
-        window.console.log("NEWS");
-        context.commit('SET_NEWS', response.data);
-        return response;
-      })
-      .catch(error => {
-        window.console.error(error);
-      });
+  async FETCH_NEWS(context) { //방법 1 에러 처리 안한것.
+    const response = await fetchNewsList();
+    context.commit('SET_NEWS', response.data);
+    return response;
   },
-  FETCH_ASK({ commit }) {
-    return fetchAskList()
-      .then(({ data }) => {
-        window.console.log("ASK");
-        commit('SET_ASK', data);
-      })
-      .catch(error => {
-        window.console.error(error);
-      });
+  async FETCH_ASK({ commit }) { //방법 2 에러 처리 한것.
+    try {
+      const response = await fetchAskList();
+      commit('SET_ASK', response.data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   },
-  FETCH_JOBS({ commit }) {  // Destructuring.. FETCH_NEWS와 비교
-    return fetchJobsList()
-      .then(({ data }) => {
-        window.console.log("JOBS");
-        commit('SET_JOBS', data);
-      })
-      .catch(error => {
-        window.console.log(error);
-      });
+  async FETCH_JOBS({ commit }) {  // Destructuring.. FETCH_NEWS와 비교
+    const response = await fetchJobsList(); //api단에서 에러 처리 => 에러를 미리 처리하고 들어와서 좀 더 깔끔하다.
+    commit('SET_JOBS', response.data);
+    return response;
   },
-  FETCH_USER({ commit }, name) {
-    return fetchUserInfo(name)
-      .then(({ data }) => {
-        window.console.log("USERS");
-        commit('SET_USER', data);
-      })
-      .catch(error => {
-        window.console.error(error);
-      });
+  async FETCH_USER({ commit }, name) {
+    const response = await fetchUserInfo(name);
+    commit('SET_USER', response.data);
+    return response;
   },
-  FETCH_ITEM({ commit }, id) {
-    return fetchCommentItem(id)
-      .then(({ data }) => {
-        commit('SET_ITEM', data);
-        window.console.log("ITEM");
-      })
-      .catch(error => {
-        window.console.log(error);
-      });
+  async FETCH_ITEM({ commit }, id) {
+    const response = await fetchCommentItem(id);
+    commit('SET_ITEM', response.data);
+    return response;
   },
-  FETCH_LIST({ commit }, pageName) {
-    return fetchList(pageName)
-      .then(response => {
-        commit('SET_LIST', response.data);
-        return response;
-      })
-      .catch((error) => window.console.log(error));
+  async FETCH_LIST({ commit }, pageName) {
+    const response = await fetchList(pageName);
+    commit('SET_LIST', response.data);
+    return response;
   }
 }
